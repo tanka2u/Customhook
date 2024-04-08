f58a57642dc0ead895b0b0891e83d432ebec64ef
tanka2u <ptanka123@gmail.com>
2024-04-08 02:36:01 +0200

    Using custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchData, setIsFetchData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setIsFetchData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+  return {
+    isFetching,
+    fetchData,
+    setIsFetchData,
+    error,
+  };
+}
